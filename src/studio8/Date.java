1f2e9dbb6f26bc253c88c0b7f483677b342b4005
cheng832 <cheng832@umn.edu>
2022-11-16 16:55:57 -0600

    Studio 8

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,93 @@
 package studio8;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Objects;
+
 public class Date {
 
+	private int Month;
+	private int Day;
+	private int Year;
+	private boolean Holiday;
+	
+	/**
+	 * Constructor
+	 * @param month
+	 * @param day
+	 * @param year
+	 * @param holiday
+	 */
+	
+	public Date(int month, int day, int year, boolean holiday) {
+		this.Month = month;
+		this.Day = day;
+		this.Year = year;
+		this.Holiday = holiday;
+	}
+	
+	/**
+	 * Getter
+	 * @return each instance variable
+	 */
+	
+	public int getmonth() {
+		return Month;
+	}
+	
+	public int getday() {
+		return Day;
+	}
+	
+	public int getyear() {
+		return Year;
+	}
+	
+	public boolean getholiday() {
+		return Holiday;
+	}
+	
+	/**
+	 * toSting method
+	 * it returns a string
+	 */
+    @Override
+	public String toString() {
+		return "Date [Month=" + Month + ", Day=" + Day + ", Year=" + Year + ", Holiday=" + Holiday + "]";
+	}
 
-    public static void main(String[] args) {
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(Day, Holiday, Month, Year);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return Day == other.Day && Holiday == other.Holiday && Month == other.Month && Year == other.Year;
+	}
+
+	public static void main(String[] args) {
+		Date x = new Date(3, 21, 2019, true);
+    	Date y = new Date(3, 21, 2019, true);
+    	System.out.println(x.equals(y));
+		LinkedList<Date> list = new LinkedList<Date>();
+		list.add(x);
+		list.add(y);
+		System.out.println(list);
+		HashSet<Date> set = new HashSet<Date>();
+		set.add(x);
+		set.add(y);
+		System.out.println(set);
+    	
+    	
     }
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,11 +1,52 @@
 package studio8;
 
+import java.util.Objects;
 
 public class Time {
+	
+	private int Hour;
+	private int Minute;
+	
+	public Time(int hour, int minute) {
+		this.Hour = hour;
+		this.Minute = minute;
+	}
+	
+	public int gethour() {
+		return Hour;
+	}
+	
+	public int getminute() {
+		return Minute;
+	}
+	
+	public String toString() {
+		return "Time [Hour = " + Hour + ", Minute = " + Minute + "]";
+	}
+	
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(Hour, Minute);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Time other = (Time) obj;
+		return Hour == other.Hour && Minute == other.Minute;
+	}
 
 	public static void main(String[] args) {
  
-    	
+    	Time y = new Time(23, 10);
+    	Time x = new Time(23, 10);
+    	System.out.println(x.equals(y));
     }
 
 }
